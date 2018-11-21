bb718dccc7b76812462d770c0cb03fe23e083354
MustafaKing5003 <42794132+MustafaKing5003@users.noreply.github.com>
2018-11-21 23:03:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
 
 client.on('ready', async() => {
 var server = "508319884724142094"; // ايدي السررفر
-var channel = ""NTE0OTAwMzUzNzkyMjEyOTk2.DtdT-w.jxrqZT1HbdmwOYt4vZoT-Ao1VnM"";//ايدي الروم
+var channel = "514905797306744832";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
